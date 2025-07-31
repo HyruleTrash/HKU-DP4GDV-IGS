5edc0198259f7f8f87525266070f604f43791631
HyruleTrash <83176601+HyruleTrash@users.noreply.github.com>
2025-07-31 15:44:12 +0200

    Update: Guns can now be random upon build (so different each save)

--- IGS_Prototype/Assets/Scripts/Guns/GunBuilder.cs
+++ IGS_Prototype/Assets/Scripts/Guns/GunBuilder.cs
@@ -1,16 +1,30 @@
 ﻿using System;
 using LucasCustomClasses;
 using UnityEngine;
+using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
 
 [CreateAssetMenu(fileName = "GunBuilder", menuName = "FPS/Guns/GunBuilder")]
 public class GunBuilder : ScriptableObject
 {
+    [Serializable]
+    public class FloatDeviationRecord
+    {
+        public bool useRandom;
+        public float maximumValue;
+        public float minimumValue;
+    }
+    
     [SerializeField] private float fireRate;
     [SerializeField] private float reloadTime;
     [SerializeField] private float baseDamage;
     [SerializeField] private int ammoCapacity;
     [SerializeField] private ScriptableObject shootStrategy;
     [SerializeField] private Vector3 bulletOrigin;
+    [Header("Random deviations")]
+    [SerializeField] private FloatDeviationRecord fireRateDeviation;
+    [SerializeField] private FloatDeviationRecord reloadTimeDeviation;
+    [SerializeField] private FloatDeviationRecord baseDamageDeviation;
     [Header("For projectile based guns")]
     [SerializeField] private float force = 0;
 
@@ -24,12 +38,23 @@ public class GunBuilder : ScriptableObject
 
     public Gun Build()
     {
-        Gun newGun = new Gun(ammoCapacity, baseDamage, force);
-        newGun.fireRateTimer = new Timer(fireRate);
-        newGun.reloadTimer = new Timer(reloadTime);
+        Gun newGun = new Gun(ammoCapacity, GetWithDeviation(baseDamage, baseDamageDeviation, f => f >= 0.1), force);
+        newGun.fireRateTimer = new Timer(GetWithDeviation(fireRate, fireRateDeviation, f => f >= 0.01f));
+        newGun.reloadTimer = new Timer(GetWithDeviation(reloadTime, reloadTimeDeviation, f => f >= 0.01f));
         newGun.shootStrategy = (IShootStrategy)shootStrategy;
         newGun.bulletOrigin = bulletOrigin;
-        // TODO: make a use for this being a builder, such as adding randomness
         return newGun;
+    }
+
+    public float GetWithDeviation(float basis, FloatDeviationRecord record, Func<float, bool> condition)
+    {
+        if (!record.useRandom)
+            return basis;
+        
+        float result = basis * Random.Range(record.minimumValue, record.maximumValue);
+        Debug.Log($"result: {result}, condition: {condition(result)}, min: {record.minimumValue}");
+        if (condition(result))
+            return result;
+        return record.minimumValue;
     }
 }
