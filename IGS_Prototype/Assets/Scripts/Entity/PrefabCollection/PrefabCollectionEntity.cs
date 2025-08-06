4dac740e0feaeadbb0c91725e8f1de1cc7998971
HyruleTrash <83176601+HyruleTrash@users.noreply.github.com>
2025-08-06 15:46:56 +0200

    Added: a start to level 0

---
+++ IGS_Prototype/Assets/Scripts/Entity/PrefabCollection/PrefabCollectionEntity.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+
+public class PrefabCollectionEntity : IEntity
+{
+    public bool Active { get; set; }
+    public GameObject Body { get; set; }
+    public void OnEnableObject()
+    {
+        Body.SetActive(true);
+    }
+
+    public void OnDisableObject()
+    {
+        Body.SetActive(false);
+    }
+
+    public void DoDie()
+    {
+        var gameReference = Game.instance;
+        gameReference.GetEntityManager().entityPool.DestroyObject(this);
+    }
+
+    public void CustomUpdate() { }
+    public void CustomUpdateAtFixedRate() { }
+}
---
+++ IGS_Prototype/Assets/Scripts/Entity/PrefabCollection/PrefabCollectionEntityData.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "PrefabCollectionEntityData", menuName = "EntityData/PrefabCollectionEntityData")]
+public class PrefabCollectionEntityData : LevelDataEntity
+{
+    [SerializeField] private GameObject prefab;
+    
+    public override IEntity Load()
+    {
+        var entityManagerReference = Game.instance.GetEntityManager();
+        PrefabCollectionEntity prefabCollectionEntity;
+        
+        prefabCollectionEntity = new PrefabCollectionEntity
+        {
+            Active = true,
+            Body = Instantiate(prefab, position, Quaternion.identity)
+        };
+
+        entityManagerReference.entityPool.AddToPool(prefabCollectionEntity);
+
+        return prefabCollectionEntity;
+    }
+}
